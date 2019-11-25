a2246f516cd42d5b3431372e037ff57693370651
Chris Wood <chris_wood@bcit.ca>
2019-11-25 12:55:15 -0800

    added config check to startup/wait script

--- 99-wait-for-mongo.sh
+++ 99-wait-for-mongo.sh
@@ -1,8 +1,12 @@
-echo $MH_MONGO_URI | while IFS=: read server port 
-do
-    while ! nc -z $server $port; do
-      echo "waiting for $server:$port to start"
-      sleep 1
-    done
-    echo "$server:$port has started"
-done
+#!/bin/sh
+# wait for mongodb to be available if using as a a storeage option
+if [ "${MH_STORAGE}" = "mongodb" ] ; then 
+  echo $MH_MONGO_URI | while IFS=: read server port 
+  do
+      while ! nc -z $server $port; do
+        echo "waiting for $server:$port to start"
+        sleep 1
+      done
+      echo "$server:$port has started"
+  done
+fi
